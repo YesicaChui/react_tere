5f99990416648cee65c37d970f6b5c77cf36e1f8
YesicaChui <yesicachuic@gmail.com>
2024-02-05 19:03:44 -0500

    estilizando contenido y sus  botones

--- src/components/Contenido.jsx
+++ src/components/Contenido.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import playStore from "../assets/play_store.png"
 import banner from "../assets/banner.png"
+import appStore from "../assets/app_store.png"
 
 const Contenido = () => {
   return (
     <div className='flex justify-between'>
       <div className='flex flex-col w-[557px] '>
-        <h1 className='text-[52px] font-[700]'>DOWNLOAD APP, SAVE MONEY, MAKE FRIENDS!</h1>
-        <p>Its simple and its free. Play your part in reducing Carbon Footprint and help Mother Nature to sustain its beauty. So what are you waiting for ?
+        <h1 className='text-[52px]  font-[700] leading-[65.91px] mt-16'>DOWNLOAD APP, SAVE MONEY, MAKE FRIENDS!</h1>
+        <p className='text-[26px] font-extralight py-10'>Its simple and its free. Play your part in reducing Carbon Footprint and help Mother Nature to sustain its beauty. So what are you waiting for ?
           Lets ride together</p>
-        <div>
-          <div>
+        <div className='flex gap-5 mt-5'>
+          <div className='flex items-center justify-center gap-3 bg-black text-white  font-semibold py-3 w-[260px] rounded-[5px] text-[24px]' >
             <img src={playStore} alt="" />
+            <p>DOWNLOAD</p>
+          </div>
+          <div className='flex items-center justify-center gap-3 bg-black text-white  font-semibold py-3 w-[260px] rounded-[5px] text-[24px]' >
+            <img src={appStore} alt="" />
             <p>DOWNLOAD</p>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'karen':['Protest Riot'],
+      }
+    },
   },
   plugins: [],
 }
